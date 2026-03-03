75a87dc3aa38b83252027c4c0e911c7cb1846257
DE-IGNIS <chauhansudarsh@gmail.com>
2026-03-03 23:34:12 +0530

    Enhance CLI output styling with chalk for improved readability

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import chalk from "chalk";
 import { createBoilerPlate } from "./setup.js";
 
 program
-  .name("api-forge")
+  .name(chalk.cyan.bold("api-forge"))
   .description(
-    `${chalk.blueBright("A CLI tool that scaffolds production ready REST & GraphQL API projects")}`,
+    chalk.blueBright(
+      "A CLI tool that scaffolds production ready REST & GraphQL API projects",
+    ),
   )
-  .version(`${chalk.greenBright("1.0.0")}`);
+  .version(chalk.greenBright.bold("1.0.0"));
 
 program
   .command("init <projectName>")
-  .description("Command for Basic API boilerplate")
-  .option("-y,--init <project>", "Project Name") // flag -> project name
+  .description(chalk.magenta("Command for Basic API boilerplate"))
+  .option("-y,--init <project>", chalk.gray("Project Name"))
   .action(createBoilerPlate);
 
 program.parse();
